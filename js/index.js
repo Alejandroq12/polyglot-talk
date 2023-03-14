983bfed811399b06728731017bdd2d6279c24445
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-13 20:15:47 -0600

    Add javascript to activate hamburger menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+const hamburgerBtn = document.querySelector('#hamburger-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.close-button');
 
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active'); // toggle active class on hamburger button
+  mobileMenu.classList.toggle('show'); // toggle show class on mobile menu
+});
+
+closeBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.remove('active'); // remove active class from hamburger button
+  mobileMenu.classList.remove('show'); // remove show class from mobile menu
+});
